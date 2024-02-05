5b98153ff673bd242aabe2e392f000439b0b9ee8
samuel udoka <shedrachsamuel4@gmail.com>
2024-02-06 00:36:42 +0100

    first commit

---
+++ src/main/java/org/applicationsmart/services/EmailServiceImpl.java
@@ -0,0 +1,2 @@
+package org.applicationsmart.services;public class EmailServiceImpl {
+}
---
+++ src/test/java/org/applicationsmart/services/UserServiceImplTest.java
@@ -0,0 +1,12 @@
+package org.applicationsmart.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class MailServiceImplTest {
+    @Autowired
+    private UserService
+
+}
